ff3e3869fd48139bff3100559b6a336fe9b47c4f
Martín Villablanca <pleasejelpmedev@outlook.com>
2024-03-24 21:35:43 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# BCV USD/EURO simple scraper
+
+Simple script que obtiene los precios del dólar y euro a la tasa BCV, y los escribe en un archivo .txt
+
+---
+## Requerimientos
+- [Python3](https://www.python.org/downloads/)
+
+## Instalación
+En la raíz del proyecto:
+```
+pip install requirements.txt
+```
+
+Luego ejecutar ```python scraper.py```
---
+++ scraper.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+import requests
+import urllib3
+
+# Attempting to scrap the websate causes an SSL error because a bad implementation of SSL certification
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+url = 'https://bcv.org.ve'
+
+try:
+    response = requests.get(url, verify=False)
+except Exception as err:
+    with open('error.txt', 'w') as file:
+        file.write(str(err))
+
+
+content = response.text
+
+soup = BeautifulSoup(content, 'lxml')
+dolar = soup.find(id='dolar').get_text(strip=True, separator=' ')
+euro = soup.find(id='euro').get_text(strip=True, separator=' ')
+
+
+with open('dolarbcv.txt', 'w') as file:
+    file.write(f'{dolar}   |    {euro}')
+    file.close()
